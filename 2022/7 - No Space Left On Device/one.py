bd565ffcf34fd73651d59e5a2c408ef0a894d89e
Donald Winkelman <dmwink.tx@gmail.com>
2022-12-11 17:14:40 -0500

    2022 7 1 debugging

--- 2022/7 - No Space Left On Device/one.py
+++ 2022/7 - No Space Left On Device/one.py
@@ -1,10 +1,44 @@
-directoryIndexMap = {}
+def containsNumber(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
+def sum(value):
+    total = 0
+    for item in value:
+        if type(item) == int:
+                total += item
+        else:
+            total += sum(dirSizes.get(item))
+    return total
+
 with open("rootDirectory.txt") as root:
+    dirStack = ['/']
+    dirSizes = {}
+    dirIntSizes = {}
     for line in root.readlines():
-        root = []
-        line.strip("\n")
-        if line.startswith("$cd "): focusedDirectory = line[4:]
-        if line.startswith("$ ls"):
-            record = True
-        while record:
-            
+        line = line.strip("\n")
+        if line.startswith("$ cd .."):
+            dirStack.pop()
+        elif line.startswith("$ cd "): 
+            dirStack.append(line[5:])
+        elif line.startswith("$ ls"):
+            dirSizes.update({str(dirStack[-1]): []})
+        else:
+            if containsNumber(line):
+                dirSizes[dirStack[-1]].append(int(line[:line.index(" ")]))
+            else:
+                dirSizes[dirStack[-1]].append(line[4:])
+
+    for key, value in dirSizes.items():
+        total = sum(value)
+        dirIntSizes.update({key: total})
+
+    print(dirIntSizes)
+
+    smallStackSize = 0
+    for value in dirIntSizes.values():
+        if value <= 100000:
+            smallStackSize += value
+    print(smallStackSize)
